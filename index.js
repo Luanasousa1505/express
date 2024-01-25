a4b8f3a0364a6ba06db9080b01dd2ca5eecb147b
Luana <Luanasousa1505@gmail.com>
2024-01-24 21:27:57 -0300

    Linhas de parametros obrigatorias e não obrigatorias

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const app = express();
 
 
 app.get("/", function(req,res){
-    res.send("Bem vindo ")
+    res.send("Bem vindo ");
 });
 
-app.get("/blog", function(req,res){
-    res.send("Bem vindo oa blog ")
+app.get("/blog/:artigo?", function(req,res){
+
+
+    var  artigo = req.params.artigo;
+     
+    if(artigo){
+        res.send("<h2>Artigo: " + artigo + "</h2>");
+    }else{
+        res.send("<h3>Bem vindo oa blog </h3>");
+    }
+    
 });
 
 app.get("/canal", function(req,res){
-    res.send("Bem vindo ao canal ")
+    res.send("Bem vindo ao canal! ");
 });
 
+
+app.get("/ola/:nome", function(req,res){
+    var nome = req.params.nome;
+
+    res.send("<h1>oi " + nome + "</h1>");
+});
 
 app.listen(4000,function(erro){
     if(erro){
